d8202cd1ef92147d400a78afb1434c96b55d65e6
Owner <Owner@DESKTOP-KU4DKRK>
2018-11-08 15:05:48 -0600

    Ryan's first commit, password hash

---
+++ MathWiz/MathWiz/PasswordHash.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+// Ryan Harpham
+// 11/8/2018
+// Source: http://csharptest.net/470/another-example-of-how-to-store-a-salted-password-hash/
+// Another source: https://stackoverflow.com/questions/4181198/how-to-hash-a-password
+
+/*
+ *  So essentially we instantiate this class, PasswordHash userPassword = new PasswordHash(password);
+ *  But the problem I'm seeing is that the _hash property is readonly, so we couldn't access it and send it to our database?
+ *  I mean, we could make the property public, do something like in stack overflow example
+ *      i.e string savedPasswordHash = Convert.ToBase64String(userPassword._hash); and send that to the DB
+ *  Other than that, the Verify function looks solid, userPassword.Verify(incomingPassword);
+*/
+
+namespace MathWiz
+{
+    public sealed class PasswordHash
+    {
+        const int SaltSize = 16, HashSize = 20, HashIter = 10000;
+        readonly byte[] _salt, _hash;
+        public PasswordHash(string password)
+        {
+            new RNGCryptoServiceProvider().GetBytes(_salt = new byte[SaltSize]);
+            _hash = new Rfc2898DeriveBytes(password, _salt, HashIter).GetBytes(HashSize);
+        }
+        public PasswordHash(byte[] hashBytes)
+        {
+            Array.Copy(hashBytes, 0, _salt = new byte[SaltSize], 0, SaltSize);
+            Array.Copy(hashBytes, SaltSize, _hash = new byte[HashSize], 0, HashSize);
+        }
+        public PasswordHash(byte[] salt, byte[] hash)
+        {
+            Array.Copy(salt, 0, _salt = new byte[SaltSize], 0, SaltSize);
+            Array.Copy(hash, 0, _hash = new byte[HashSize], 0, HashSize);
+        }
+        public byte[] ToArray()
+        {
+            byte[] hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(_salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(_hash, 0, hashBytes, SaltSize, HashSize);
+            return hashBytes;
+        }
+        public byte[] Salt { get { return (byte[])_salt.Clone(); } }
+        public byte[] Hash { get { return (byte[])_hash.Clone(); } }
+        public bool Verify(string password)
+        {
+            byte[] test = new Rfc2898DeriveBytes(password, _salt, HashIter).GetBytes(HashSize);
+            for (int i = 0; i < HashSize; i++)
+                if (test[i] != _hash[i])
+                    return false;
+            return true;
+        }
+    }
+}
