98c41903322879d6190cd9a3c76f4a81a1cf98f9
Kevin Van Cott <kevinvandy656@gmail.com>
2018-11-09 15:12:51 -0600

    Created most of the classes with no attributes yet

---
+++ MathWiz/MathWiz/MathWizDA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathWiz
+{
+    //Class for reading (selecting) information from the database
+    class MathWizDA
+    {
+    }
+}
---
+++ MathWiz/MathWiz/MathWizDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathWiz
+{
+    //Class for inserting, updating, or deleting from the database
+    class MathWizDB
+    {
+    }
+}
