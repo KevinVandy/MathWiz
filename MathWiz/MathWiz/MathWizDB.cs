66a8b82ac3d2a903a58369aa3a120f6d7b78a5ea
Owner <Owner@DESKTOP-KU4DKRK>
2018-11-14 11:18:23 -0600

    Reformatted insert statements to work with new tables

--- MathWiz/MathWiz/MathWizDB.cs
+++ MathWiz/MathWiz/MathWizDB.cs
@@ -15,7 +15,7 @@ namespace MathWiz
 
         private void InsertParent(Parent newParent)
         {
-            string insertStatement = "INSERT INTO parent (Username, FirstName, LastName, PasswordHash) " +
+            string insertStatement = "INSERT INTO parents (Username, FirstName, LastName, PasswordHash) " +
                 "VALUES(@Username, @FirstName, @LastName, @Password)";
       
             // create command object with SQL query and link to connection object
@@ -52,7 +52,7 @@ namespace MathWiz
 
         private void InsertStudent(Student newStudent)
         {
-            string insertStatement = "INSERT INTO student (Username, FirstName, LastName, PasswordHash) " +
+            string insertStatement = "INSERT INTO students (Username, FirstName, LastName, PasswordHash) " +
                 "VALUES(@Username, @FirstName, @LastName, @Password)";
 
             // create command object with SQL query and link to connection object
@@ -89,7 +89,7 @@ namespace MathWiz
 
         private void InsertTeacher(Teacher newTeacher)
         {
-            string insertStatement = "INSERT INTO teacher (Username, FirstName, LastName, PasswordHash) " +
+            string insertStatement = "INSERT INTO teachers (Username, FirstName, LastName, PasswordHash) " +
                 "VALUES(@Username, @FirstName, @LastName, @Password)";
 
             // create command object with SQL query and link to connection object
@@ -126,7 +126,7 @@ namespace MathWiz
 
         private void InsertAdmin(Admin newAdmin)
         {
-            string insertStatement = "INSERT INTO admin (Username, FirstName, LastName, PasswordHash) " +
+            string insertStatement = "INSERT INTO admins (Username, FirstName, LastName, PasswordHash) " +
                 "VALUES(@Username, @FirstName, @LastName, @Password)";
 
             // create command object with SQL query and link to connection object
@@ -161,134 +161,179 @@ namespace MathWiz
             }
         }
 
-        private void InsertMasteryTest(MasteryTest newTest)
+        private void InsertQuestion(Question newQuestion, int testID)
         {
-            string insertStatement = "INSERT INTO mastery_test (TimeLimit, RandomlyGenerated, PassThreshold) " +
-                "VALUES(@TimeLimit, @RNG, @PassThreshold)";
-
-            // create command object with SQL query and link to connection object
-            SqlCommand Cmd = new SqlCommand(insertStatement, conn);
-
-            // create your parameters and add values from object
-            Cmd.Parameters.AddWithValue("@TimeLimit", newTest.TimeLimit);
-            Cmd.Parameters.AddWithValue("@RNG", newTest.RandomlyGenerated);
-            Cmd.Parameters.AddWithValue("@PassThreshold", newTest.PassThreshhold);
-
-            try
-            {
-                // Open the connection
-                conn.Open();
-
-                // execute the query
-                Cmd.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                // always close the connection
-                conn.Close();
-            }
-        }
-
-        private void InsertPlacementTest(PlacementTest newTest)
-        {
-            string insertStatement = "INSERT INTO placement_test (TimeLimit, RandomlyGenerated, MinLevel, MaxLevel) " +
-                "VALUES(@TimeLimit, @RNG, @Min, @Max)";
-
-            // create command object with SQL query and link to connection object
-            SqlCommand Cmd = new SqlCommand(insertStatement, conn);
-
-            // create your parameters and add values from object
-            Cmd.Parameters.AddWithValue("@TimeLimit", newTest.TimeLimit);
-            Cmd.Parameters.AddWithValue("@RNG", newTest.RandomlyGenerated);
-            Cmd.Parameters.AddWithValue("@Min", newTest.MinLevel);
-            Cmd.Parameters.AddWithValue("@Max", newTest.MaxLevel);
-
-            try
-            {
-                // Open the connection
-                conn.Open();
-
-                // execute the query
-                Cmd.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                // always close the connection
-                conn.Close();
-            }
-        }
-
-        private void InsertPracticeTest(PracticeTest newTest)
-        {
-            string insertStatement = "INSERT INTO placement_test (TimeLimit, RandomlyGenerated, MinLevel, MaxLevel) " +
-                "VALUES(@TimeLimit, @RNG, @Min, @Max)";
-
-            // create command object with SQL query and link to connection object
-            SqlCommand Cmd = new SqlCommand(insertStatement, conn);
-
-            // create your parameters and add values from object
-            Cmd.Parameters.AddWithValue("@TimeLimit", newTest.TimeLimit);
-            Cmd.Parameters.AddWithValue("@RNG", newTest.RandomlyGenerated);
-            Cmd.Parameters.AddWithValue("@Min", newTest.MinLevel);
-            Cmd.Parameters.AddWithValue("@Max", newTest.MaxLevel);
-
-            try
-            {
-                // Open the connection
-                conn.Open();
-
-                // execute the query
-                Cmd.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                // always close the connection
-                conn.Close();
-            }
-        }
-
-        private void InsertQuestion(Question newQuestion, Test aTest)
-        {
-            string insertStatement = "INSERT INTO question (TestID, MasteryLevel, QuestionText, CorrectAnswer, TimeLimit, Weight, RandomlyGenerated) " +
+            string insertStatement = "INSERT INTO questions (TestID, MasteryLevel, QuestionText, CorrectAnswer, TimeLimit, Weight, RandomlyGenerated) " +
                 "VALUES(@TestID, @MasteryLevel, @QuestionText, @CorrectAnswer, @TimeLimit, @Weight, @RNG)";
 
             // create command object with SQL query and link to connection object
             SqlCommand Cmd = new SqlCommand(insertStatement, conn);
 
             // create your parameters and add values from object
-            // Where am I grabbing TestID, None of our Tests have ID properties
-            // - Ryan
-            Cmd.Parameters.AddWithValue("@TestID", newQuestion);
+            Cmd.Parameters.AddWithValue("@TestID", testID);
             Cmd.Parameters.AddWithValue("@MasteryLevel", newQuestion.MasteryLevel);
             Cmd.Parameters.AddWithValue("@QuestionText", newQuestion.QuestionText);
             Cmd.Parameters.AddWithValue("@CorrectAnswer", newQuestion.CorrectAnswer);
             Cmd.Parameters.AddWithValue("@TimeLimit", newQuestion.TimeLimit);
             Cmd.Parameters.AddWithValue("@Weight", newQuestion.Weight);
             Cmd.Parameters.AddWithValue("@RNG", newQuestion.RandomlyGenerated);
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // execute the query
+                Cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // always close the connection
+                conn.Close();
+            }
+        }
+
+        private void InsertKlass(Klass newKlass, int teacherID)
+        {
+            string insertStatement = "INSERT INTO klasses (TeacherID, KlassName) " +
+                "VALUES(@TeacherID, @KlassName)";
+
+            // create command object with SQL query and link to connection object
+            SqlCommand Cmd = new SqlCommand(insertStatement, conn);
+
+            // create your parameters and add values from object
+            Cmd.Parameters.AddWithValue("@TeacherID", teacherID);
+            Cmd.Parameters.AddWithValue("@KlassName", newKlass.KlassName);
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // execute the query
+                Cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // always close the connection
+                conn.Close();
+            }
+        }
+
+        private void InsertTest(Test newTest, string testType, decimal passThreshold, int minLevel, int maxLevel)
+        {
+            //Everything besides TestType can be null
+            string insertStatement = "INSERT INTO tests (TestType, TimeLimit, RandomlyGenerated, PassThreshHold, MinLevel, MaxLevel) " +
+                "VALUES(@Type, @TimeLimit, @RNG, @PassThreshold, @MinLevel, @MaxLevel)";
+
+            // create command object with SQL query and link to connection object
+            SqlCommand Cmd = new SqlCommand(insertStatement, conn);
+
+            // create your parameters and add values from object
+            Cmd.Parameters.AddWithValue("@Type", testType);
+            Cmd.Parameters.AddWithValue("@TimeLimit", newTest.TimeLimit);
+            Cmd.Parameters.AddWithValue("@RNG", newTest.RandomlyGenerated);
+            Cmd.Parameters.AddWithValue("@PassThreshold", passThreshold);
+            Cmd.Parameters.AddWithValue("@MinLevel", minLevel);
+            Cmd.Parameters.AddWithValue("@MaxLevel", maxLevel);
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // execute the query
+                Cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // always close the connection
+                conn.Close();
+            }
+        }
+
+        private void InsertGradedQuestion(GradedQuestion newQuestion, int questionID, int gradedTestID)
+        {
+            string insertStatement = "INSERT INTO graded_questions (GradedTestID, QuestionID, StudentAnswer, Correct, TimeTakenToAnswer) " +
+                "VALUES(@TestID, @QuestionID, @StudentAnswer, @Correct, @Time)";
+
+            // create command object with SQL query and link to connection object
+            SqlCommand Cmd = new SqlCommand(insertStatement, conn);
+
+            // create your parameters and add values from object
+            Cmd.Parameters.AddWithValue("@TestID", gradedTestID);
+            Cmd.Parameters.AddWithValue("@QuestionID", questionID);
+            Cmd.Parameters.AddWithValue("@StudentAnswer", newQuestion.StudentAnswer);
+            Cmd.Parameters.AddWithValue("@Correct", newQuestion.Correct);
+            Cmd.Parameters.AddWithValue("@Time", newQuestion.TimeTakenToAnswer);
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // execute the query
+                Cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // always close the connection
+                conn.Close();
+            }
+        }
+
+        private void InsertGradedTest(GradedTest newTest, int studentID, int testID, int attempts, byte passed, int recommendedLevel)
+        {
+            // StudentID, TestID, and Score must be present
+            // Everything else can be null
+            string insertStatement = "INSERT INTO graded_tests (StudentID, TestID, Score, TimeTakenToComplete, DateTaken, Feedback, NumberAttempts, Passed, RecommendedLevel) " +
+                "VALUES(@StudentID, @TestID, @Score, @Time, @DateTaken, @Feedback, @Attempts, @Passed, @RecommendLevel)";
+
+            // create command object with SQL query and link to connection object
+            SqlCommand Cmd = new SqlCommand(insertStatement, conn);
+
+            // create your parameters and add values from object
+            Cmd.Parameters.AddWithValue("@StudentID", studentID);
+            Cmd.Parameters.AddWithValue("@TestID", testID);
+            Cmd.Parameters.AddWithValue("@Score", newTest.Score);
+            Cmd.Parameters.AddWithValue("@Time", newTest.TimeTakenToComplete);
+            Cmd.Parameters.AddWithValue("@DateTaken", newTest.DateTaken);
+            Cmd.Parameters.AddWithValue("@Feedback", newTest.Feedback);
+            Cmd.Parameters.AddWithValue("@Attempts", attempts);
+            Cmd.Parameters.AddWithValue("@Passed", passed);
+            Cmd.Parameters.AddWithValue("@RecommendLevel", recommendedLevel);
 
             try
             {
