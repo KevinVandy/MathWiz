03b1642e7e0b9043fd42101416764374c41e908d
Tyler Sweeney <tsweeney21@gmail.com>
2018-11-14 19:20:01 -0600

    Work on validation

--- MathWiz/MathWiz/Validation.cs
+++ MathWiz/MathWiz/Validation.cs
@@ -7,6 +7,10 @@ using System.Windows.Forms;
 
 namespace MathWiz
 {
+    /*Tyler's Note, I wrote a few generic validation methods here. If you think up anything else that needs validated
+     * then just let me know, and I'll write another method. The validation I wrote usually asks you to pass the entire
+     * control for validation. It makes it easier to manipulate. In the case of checkbox and radio button validation 
+     * pass the enitre groupbox containing those controls*/
     public class Validation
     {
         bool theTruth;
@@ -81,6 +85,28 @@ namespace MathWiz
             }
             return isNumeric;
             
+        }
+
+        public bool IsComboSelected(ComboBox cboBox)
+        {
+            if(cboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show(cboBox.Tag.ToString() + " must have an item selected.", "Input Error");
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsStringMaxLengthInclusive(TextBox txtBox, int maxLength)
+        {
+            int stringLength = txtBox.Text.Length;
+            if(stringLength > maxLength)
+            {
+                MessageBox.Show(txtBox.Tag.ToString() + " has a character limit of " + maxLength.ToString(), "Input Error");
+                return false;
+            }
+
+            return true; 
         }
     }
 }
