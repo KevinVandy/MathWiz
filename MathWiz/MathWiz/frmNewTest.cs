334928f4fb1d1b9d7ad96895151b434adebb0717
Tyler Sweeney <tsweeney21@gmail.com>
2018-11-20 05:53:35 -0600

    Getting tests created, and assined to the DB works. Now if you create a test from the teachers form and select min/max/mastery level 1 it will randomly generate simple additon quesitons, and read them back to you in message boxes. Don't select anything else for the time being it will probably crash the program : )

--- MathWiz/MathWiz/frmNewTest.cs
+++ MathWiz/MathWiz/frmNewTest.cs
@@ -19,8 +19,12 @@ namespace MathWiz
 
         private void btnAccept_Click(object sender, EventArgs e)
         {
+            Int64 aNumber = 0;
+
+            //if form validation is good then
             if(ValidateForm())
             {
+                //set values to build the test object based on which test is selected
                 int min = 0;
                 int max = 0;
                 decimal threshold = 0.0m;
@@ -40,30 +44,51 @@ namespace MathWiz
                 bool isTrue = Convert.ToBoolean(cboRandom.SelectedIndex);
                 
                 
-                
+                //insert the test in to the DB get a number back with the primary key ID so we can generate questions for the test
                 int caseswitch = cboType.SelectedIndex;
                 switch (caseswitch)
                 {
                     case 0:
                         PracticeTest practiceTest = new PracticeTest(qL, time, isTrue, min, max);
                         MathWizDB.InsertPracticeTest(practiceTest);
+                        aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
+                        //need to resovle how we want to generate questions
+                        practiceTest.Questions = Question.GenerateRandomQuestions(practiceTest.MaxLevel, practiceTest.TimeLimit);
+                        foreach(Question q in practiceTest.Questions)
+                        {
+                            MessageBox.Show(q.QuestionText + q.CorrectAnswer);
+                        }
                         break;
                     case 1:
-                        MessageBox.Show("Case 2");
                         PlacementTest PlacementTest = new PlacementTest(qL, time, isTrue, min, max);
                         MathWizDB.InsertPlacementTest(PlacementTest);
+                        aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
+                        //need to resolve the how to create questions
+                        PlacementTest.Questions = Question.GenerateRandomQuestions(PlacementTest.MaxLevel, PlacementTest.TimeLimit);
+                        foreach (Question q in PlacementTest.Questions)
+                        {
+                            MessageBox.Show(q.QuestionText + q.CorrectAnswer);
+                        }
                         break;
                     case 2:
                         MasteryTest masteryTest = new MasteryTest(qL, time, isTrue, masteryLevel, threshold);
                         MathWizDB.InsertMasteryTest(masteryTest);
+                        aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
+                        //need to resolve the how on creatign questions
+                        masteryTest.Questions = Question.GenerateRandomQuestions(masteryTest.MasteryLevel, masteryTest.TimeLimit);
+                        foreach (Question q in masteryTest.Questions)
+                        {
+                            MessageBox.Show(q.QuestionText + q.CorrectAnswer);
+                        }
                         break;
 
 
                 }
 
+
             }
         }
-
+        //validation methods for the form based on which test is selected
         private bool ValidateForm()
         {
             if(cboMinimum.Enabled == true)
@@ -96,7 +121,7 @@ namespace MathWiz
             }
 
         }
-
+        //dynamically updates the controls on the form based on which test is selected
         private void cboType_SelectedIndexChanged(object sender, EventArgs e)
         {
             int caseswtich = cboType.SelectedIndex;
@@ -127,8 +152,7 @@ namespace MathWiz
 
         private void frmNewTest_Load(object sender, EventArgs e)
         {
-            Int64 aNumber = MathWizDA.GetLastInsertedRecord("tests", "id");
-            MessageBox.Show(aNumber.ToString());
+
         }
     }
 }
