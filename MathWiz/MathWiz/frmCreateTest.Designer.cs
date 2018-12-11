0fe9b0c0b998f05e5993ec293bc53c0aec8cc0ee
fb671687 <fb671687@LR00117>
2018-12-11 11:14:35 -0600

    Placement Test form

--- MathWiz/MathWiz/frmCreateTest.Designer.cs
+++ MathWiz/MathWiz/frmCreateTest.Designer.cs
@@ -29,197 +29,53 @@
         private void InitializeComponent()
         {
             this.btnCreateTest = new System.Windows.Forms.Button();
-            this.cboType = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.lblMaxMaster = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.mskTime = new System.Windows.Forms.MaskedTextBox();
-            this.mskThreshold = new System.Windows.Forms.MaskedTextBox();
-            this.cboMinimum = new System.Windows.Forms.ComboBox();
-            this.cboMaximum = new System.Windows.Forms.ComboBox();
-            this.cboRandom = new System.Windows.Forms.ComboBox();
             this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateTest
             // 
             this.btnCreateTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreateTest.Location = new System.Drawing.Point(260, 356);
+            this.btnCreateTest.Location = new System.Drawing.Point(345, 151);
+            this.btnCreateTest.Margin = new System.Windows.Forms.Padding(4);
             this.btnCreateTest.Name = "btnCreateTest";
-            this.btnCreateTest.Size = new System.Drawing.Size(157, 52);
+            this.btnCreateTest.Size = new System.Drawing.Size(209, 64);
             this.btnCreateTest.TabIndex = 0;
             this.btnCreateTest.Text = "Cre&ate Test";
             this.btnCreateTest.UseVisualStyleBackColor = true;
             this.btnCreateTest.Click += new System.EventHandler(this.btnCreateTest_Click);
             // 
-            // cboType
-            // 
-            this.cboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboType.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cboType.FormattingEnabled = true;
-            this.cboType.Items.AddRange(new object[] {
-            "Practice Test",
-            "Placement Test",
-            "Mastery Test"});
-            this.cboType.Location = new System.Drawing.Point(241, 33);
-            this.cboType.Name = "cboType";
-            this.cboType.Size = new System.Drawing.Size(176, 32);
-            this.cboType.TabIndex = 1;
-            this.cboType.Tag = "Test Type";
-            this.cboType.SelectedIndexChanged += new System.EventHandler(this.cboType_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(118, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 24);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Test Type:";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 87);
+            this.label2.Location = new System.Drawing.Point(33, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(195, 24);
+            this.label2.Size = new System.Drawing.Size(250, 29);
             this.label2.TabIndex = 3;
             this.label2.Text = "Time Limit (hh:mm:ss)";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(71, 129);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(146, 24);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Pass Threshold:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(78, 177);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(143, 24);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Minimum Level:";
-            // 
-            // lblMaxMaster
-            // 
-            this.lblMaxMaster.AutoSize = true;
-            this.lblMaxMaster.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMaxMaster.Location = new System.Drawing.Point(78, 226);
-            this.lblMaxMaster.Name = "lblMaxMaster";
-            this.lblMaxMaster.Size = new System.Drawing.Size(143, 24);
-            this.lblMaxMaster.TabIndex = 6;
-            this.lblMaxMaster.Text = "Maximum Level";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(22, 284);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(199, 24);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "Randomly Generated?";
             // 
             // mskTime
             // 
             this.mskTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mskTime.Location = new System.Drawing.Point(241, 82);
+            this.mskTime.Location = new System.Drawing.Point(321, 40);
+            this.mskTime.Margin = new System.Windows.Forms.Padding(4);
             this.mskTime.Mask = "00:00:00";
             this.mskTime.Name = "mskTime";
-            this.mskTime.Size = new System.Drawing.Size(176, 29);
+            this.mskTime.Size = new System.Drawing.Size(233, 34);
             this.mskTime.TabIndex = 9;
             this.mskTime.Tag = "Time Limit";
-            // 
-            // mskThreshold
-            // 
-            this.mskThreshold.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mskThreshold.Location = new System.Drawing.Point(241, 126);
-            this.mskThreshold.Mask = "00.000000";
-            this.mskThreshold.Name = "mskThreshold";
-            this.mskThreshold.Size = new System.Drawing.Size(176, 29);
-            this.mskThreshold.TabIndex = 10;
-            this.mskThreshold.Tag = "Pass Threshold";
-            // 
-            // cboMinimum
-            // 
-            this.cboMinimum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboMinimum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cboMinimum.FormattingEnabled = true;
-            this.cboMinimum.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12"});
-            this.cboMinimum.Location = new System.Drawing.Point(241, 174);
-            this.cboMinimum.Name = "cboMinimum";
-            this.cboMinimum.Size = new System.Drawing.Size(176, 32);
-            this.cboMinimum.TabIndex = 11;
-            this.cboMinimum.Tag = "Minimum Level";
-            // 
-            // cboMaximum
-            // 
-            this.cboMaximum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboMaximum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cboMaximum.FormattingEnabled = true;
-            this.cboMaximum.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12"});
-            this.cboMaximum.Location = new System.Drawing.Point(241, 223);
-            this.cboMaximum.Name = "cboMaximum";
-            this.cboMaximum.Size = new System.Drawing.Size(176, 32);
-            this.cboMaximum.TabIndex = 12;
-            this.cboMaximum.Tag = "Maximum Level";
-            // 
-            // cboRandom
-            // 
-            this.cboRandom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cboRandom.FormattingEnabled = true;
-            this.cboRandom.Items.AddRange(new object[] {
-            "Yes",
-            "No"});
-            this.cboRandom.Location = new System.Drawing.Point(241, 281);
-            this.cboRandom.Name = "cboRandom";
-            this.cboRandom.Size = new System.Drawing.Size(176, 32);
-            this.cboRandom.TabIndex = 13;
-            this.cboRandom.Tag = "Ranodmly Generated";
             // 
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(57, 356);
+            this.btnCancel.Location = new System.Drawing.Point(64, 151);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(164, 52);
+            this.btnCancel.Size = new System.Drawing.Size(219, 64);
             this.btnCancel.TabIndex = 14;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -228,27 +84,17 @@
             // frmCreateTest
             // 
             this.AcceptButton = this.btnCreateTest;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(484, 468);
+            this.ClientSize = new System.Drawing.Size(645, 264);
             this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.cboRandom);
-            this.Controls.Add(this.cboMaximum);
-            this.Controls.Add(this.cboMinimum);
-            this.Controls.Add(this.mskThreshold);
             this.Controls.Add(this.mskTime);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.lblMaxMaster);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cboType);
             this.Controls.Add(this.btnCreateTest);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmCreateTest";
-            this.Text = "Create New Test";
-            this.Load += new System.EventHandler(this.frmCreateTest_Load);
+            this.Text = "Create Placement Test";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,18 +103,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnCreateTest;
-        private System.Windows.Forms.ComboBox cboType;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lblMaxMaster;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.MaskedTextBox mskTime;
-        private System.Windows.Forms.MaskedTextBox mskThreshold;
-        private System.Windows.Forms.ComboBox cboMinimum;
-        private System.Windows.Forms.ComboBox cboMaximum;
-        private System.Windows.Forms.ComboBox cboRandom;
         private System.Windows.Forms.Button btnCancel;
     }
 }
--- MathWiz/MathWiz/frmCreateTest.cs
+++ MathWiz/MathWiz/frmCreateTest.cs
@@ -28,141 +28,26 @@ namespace MathWiz
             if(ValidateForm())
             {
                 //set values to build the test object based on which test is selected
-                int min = 0;
-                int max = 0;
-                decimal threshold = 0.0m;
-                int masteryLevel = 0;
-                if (cboMinimum.Enabled == true)
-                {
-                    min = cboMinimum.SelectedIndex + 1;
-                    max = cboMaximum.SelectedIndex + 1;
-                }
-                else
-                {
-                    threshold = Convert.ToDecimal(mskThreshold.Text);
-                    masteryLevel = cboMaximum.SelectedIndex + 1;
-                }
+                int min = 1;
+                int max = 12;
                 List<Question> qL = new List<Question>();
                 TimeSpan time = TimeSpan.Parse(mskTime.Text);
-                bool isTrue = Convert.ToBoolean(cboRandom.SelectedIndex);
-                
-                
-                //insert the test in to the DB get a number back with the primary key ID so we can generate questions for the test
-                int caseswitch = cboType.SelectedIndex;
-                switch (caseswitch)
-                {
-                    //Case Practice test
-                    case 0:
-                        PracticeTest practiceTest = new PracticeTest(qL, time, isTrue, min, max);
-                        MathWizDB.InsertPracticeTest(practiceTest, klassID);
-                        aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
-                        //need to resovle how we want to generate questions
-                        this.Close();
-                        practiceTest.Questions = Question.GenerateRandomQuestions(practiceTest.MaxLevel, practiceTest.TimeLimit,10);
-                        foreach(Question q in practiceTest.Questions)
-                        {
-                            MathWizDB.InsertTestQuestionsPractice(practiceTest, q, aNumber);
-                        }
-                        MessageBox.Show("Your new randomly generated Practice test has been created");
-                        break;
-
-
-
-                    //case Placement Test
-                    case 1:
-                        PlacementTest PlacementTest = new PlacementTest(qL, time, isTrue, min, max);
-                        MathWizDB.InsertPlacementTest(PlacementTest, klassID);
-                        aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
-                        //need to resolve the how to create questions
-                        this.Close();
-                        PlacementTest.Questions = Question.GenerateRandomQuestions(PlacementTest.MaxLevel, PlacementTest.TimeLimit, 10);
-                        foreach (Question q in PlacementTest.Questions)
-                        {
-                            MathWizDB.InsertTestQuestionPlacement(PlacementTest, q, aNumber);
-                        }
-                        MessageBox.Show("Your new randomly generated Placement test has been created");
-                        break;
-
-
-
-                    //case Mastery Test
-                    case 2:
-                        if (MathWizDA.SelectKlassesMasteryLevels(klassID, masteryLevel) == false)
-                        {
-                            MasteryTest masteryTest = new MasteryTest(qL, time, isTrue, masteryLevel, threshold);
-                            MathWizDB.InsertMasteryTest(masteryTest, klassID);
-                            aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
-                            //need to resolve the how on creating questions
-                            this.Close();
-                            masteryTest.Questions = Question.GenerateRandomQuestions(masteryTest.MasteryLevel, masteryTest.TimeLimit, 10);
-                            foreach (Question q in masteryTest.Questions)
-                            {
-                                MathWizDB.InsertTestQuestionMastery(masteryTest, q, aNumber);
-                            }
-                            MessageBox.Show("Your new randomly generated mastery test has been created");
-                            break;
-                        } else
-                        {
-                            MessageBox.Show("A mastery test has already been generated for this level");
-                            break;
-                        }
-                }
-            }
-        }
-        //validation methods for the form based on which test is selected
-        private bool ValidateForm()
-        {
-            if(cboMinimum.Enabled == true)
+                                                    
+                //Placement Test
+                PlacementTest PlacementTest = new PlacementTest(qL, time, min, max);
+                MathWizDB.InsertPlacementTest(PlacementTest, klassID);
+                aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
+                //need to resolve the how to create questions
+                 this.Close();
+                 PlacementTest.Questions = Question.GenerateRandomQuestions(PlacementTest.MaxLevel, PlacementTest.TimeLimit, 10);
+                 foreach (Question q in PlacementTest.Questions)
+                 {
+                     MathWizDB.InsertTestQuestionPlacement(PlacementTest, q, aNumber);
+                 }
+                 MessageBox.Show("Your new randomly generated Placement test has been created");
+                } else
             {
-                if(Validation.IsComboSelected(cboType) &&
-               Validation.IsMaskPresent(mskTime) &&
-               Validation.IsComboSelected(cboMinimum) &&
-               Validation.IsComboSelected(cboMaximum) &&
-               Validation.IsComboSelected(cboRandom) &&
-               Validation.secondIndexNotLower(cboMinimum, cboMaximum) == true)
-               {
-                    return true;
-                }
-
-                return false;
-            }
-            else
-            {
-                if(Validation.IsComboSelected(cboType) &&
-               Validation.IsMaskPresent(mskTime) &&
-               Validation.IsMaskPresent(mskThreshold) &&
-               Validation.IsComboSelected(cboMaximum) &&
-               Validation.IsComboSelected(cboRandom) &&
-               Validation.secondIndexNotLower(cboMinimum, cboMaximum) == true)
-                {
-                        return true;
-                }
-
-                return false;
-            }
-
-        }
-        //dynamically updates the controls on the form based on which test is selected
-        private void cboType_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int caseswtich = cboType.SelectedIndex;
-            switch(caseswtich)
-            {
-                case 0:
-                    cboMinimum.Enabled = true;
-                    lblMaxMaster.Text = "Maximum Level:";
-                    mskThreshold.Enabled = false;
-                    break;
-                case 1:
-                    cboMinimum.Enabled = true;
-                    lblMaxMaster.Text = "Maximum Level:";
-                    mskThreshold.Enabled = false;
-                    break;
-                case 2:
-                    cboMinimum.Enabled = false;
-                    lblMaxMaster.Text = "Mastery Level:";
-                    mskThreshold.Enabled = true;
-                    break;
+                MessageBox.Show("A placement test has already been created for this class.");
             }
         }
 
@@ -171,9 +56,16 @@ namespace MathWiz
             this.Close();
         }
 
-        private void frmCreateTest_Load(object sender, EventArgs e)
+    //validation methods for the form based on which test is selected
+    private bool ValidateForm()
         {
+               if(Validation.IsMaskPresent(mskTime) && MathWizDA.FindPlacementTest(klassID) == false )
+                {
+                        return true;
+                }
 
-        }
+                return false;
+            }
     }
+        
 }
