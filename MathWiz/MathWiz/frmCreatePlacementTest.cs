cee4971bb599cea9e54d68a90abd922121064a4d
Kevin Van Cott <kevinvandy656@gmail.com>
2018-12-12 16:00:17 -0600

    just publishing my progress so far on frmCreatePlacementTest

--- MathWiz/MathWiz/frmCreatePlacementTest.cs
+++ MathWiz/MathWiz/frmCreatePlacementTest.cs
@@ -27,30 +27,45 @@ namespace MathWiz
 
         private void btnCreateTest_Click(object sender, EventArgs e)
         {
-            Int64 aNumber = 0;
-
             //if form validation is good then
-            if(ValidateForm())
+            if (ValidateForm())
             {
-                //set values to build the test object based on which test is selected
-                int min = 1;
-                int max = 12;
-                List<Question> qL = new List<Question>();
-                TimeSpan time = TimeSpan.Parse(mskTime.Text);
-                                                    
-                //Placement Test
-                PlacementTest PlacementTest = new PlacementTest(qL, time, min, max);
-                MathWizDB.InsertPlacementTest(PlacementTest, klassID);
-                aNumber = MathWizDA.GetLastInsertedRecord("test", "id");
-                //need to resolve the how to create questions
-                 this.Close();
-                 PlacementTest.Questions = Question.GenerateRandomQuestions(PlacementTest.MaxLevel, PlacementTest.TimeLimit, 10);
-                 foreach (Question q in PlacementTest.Questions)
-                 {
-                     MathWizDB.InsertTestQuestionPlacement(PlacementTest, q, aNumber);
-                 }
-                 MessageBox.Show("Your new randomly generated Placement test has been created");
+                int numberOfQuestions = Convert.ToInt32(cboNumberOfQuestions.SelectedItem);
+                int numberOfQuestionsPerML = numberOfQuestions / 12; //take a few questions from each mastery level
+                TimeSpan questionTimeLimit = new TimeSpan(0,1,0);
+
+                PlacementTest placementTest = new PlacementTest();
+                
+                for (int ml = 1; ml <= 12; ml++) //get questions from each mastery level
+                {
+                    //make custom timelimits for each question depending on complexity
+                    questionTimeLimit = CalculateQuestionTimeLimit(cboQuestionSpeed.SelectedItem.ToString(), ml);
+
+                    //get the questions for just the current mastery level in this for loop
+                    List<Question> tempQuestionList = Question.GenerateRandomQuestions(ml, questionTimeLimit, numberOfQuestionsPerML);
+
+                    foreach (Question q in tempQuestionList) //add current mastery level questions to the actual placement test
+                    {
+                        placementTest.Questions.Add(q);
+                    }
                 }
+
+                //set other attributes of the test
+                placementTest.MinLevel = 1;
+                placementTest.MaxLevel = 12;
+                placementTest.TimeLimit = TimeSpan.FromTicks(questionTimeLimit.Ticks * numberOfQuestions);
+
+                //insert it
+                placementTest.Id = MathWizDB.InsertTest(klassID, placementTest, "Placement Test", 0, placementTest.MinLevel, placementTest.MaxLevel);
+
+                this.Close();
+
+                foreach (Question q in placementTest.Questions)
+                {
+                    MathWizDB.InsertTestQuestionPlacement(placementTest, q, placementTest.Id);
+                }
+                MessageBox.Show("Your new randomly generated Placement test has been created");
+            }
             else
             {
                 MessageBox.Show("A placement test has already been created for this class.");
@@ -62,18 +77,80 @@ namespace MathWiz
             this.Close();
         }
 
-    //validation methods for the form based on which test is selected
-    private bool ValidateForm()
+        private TimeSpan CalculateQuestionTimeLimit(string speed, int ml)
         {
-               if(Validation.IsMaskPresent(mskTime) && MathWizDA.FindPlacementTest(klassID) == false )
-                {
-                        return true;
-                }
+            switch (speed) //figure out the time limit for each question
+            {
+                case "Slow":
 
-                return false;
+                    if (ml >= 1 && ml <= 3)
+                    {
+                        return new TimeSpan(0, 0, 45);
+                    }
+                    else if (ml >= 4 && ml <= 6)
+                    {
+                        return new TimeSpan(0, 1, 0);
+                    }
+                    else if (ml >= 7 && ml <= 9)
+                    {
+                        return new TimeSpan(0, 0, 45);
+                    }
+                    else if (ml >= 10 && ml <= 12)
+                    {
+                        return new TimeSpan(0, 3, 0);
+                    }
+
+                    break;
+
+                case "Medium":
+
+                    if (ml >= 1 && ml <= 3)
+                    {
+                        return new TimeSpan(0, 0, 30);
+                    }
+                    else if (ml >= 4 && ml <= 6)
+                    {
+                        return new TimeSpan(0, 0, 45);
+                    }
+                    else if (ml >= 7 && ml <= 9)
+                    {
+                        return new TimeSpan(0, 0, 30);
+                    }
+                    else if (ml >= 10 && ml <= 12)
+                    {
+                        return new TimeSpan(0, 2, 0);
+                    }
+
+                    break;
+
+                case "Fast":
+
+                    if (ml >= 1 && ml <= 3)
+                    {
+                        return new TimeSpan(0, 0, 15);
+                    }
+                    else if (ml >= 4 && ml <= 6)
+                    {
+                        return new TimeSpan(0, 0, 30);
+                    }
+                    else if (ml >= 7 && ml <= 9)
+                    {
+                        return new TimeSpan(0, 0, 15);
+                    }
+                    else if (ml >= 10 && ml <= 12)
+                    {
+                        return new TimeSpan(0, 1, 0);
+                    }
+
+                    break;
             }
+            return new TimeSpan(0, 1, 0); //default
+        }
 
-        
-    }
-        
+        //validation methods for the form based on which test is selected
+        private bool ValidateForm()
+        {
+            return true;
+        }
+    } 
 }
